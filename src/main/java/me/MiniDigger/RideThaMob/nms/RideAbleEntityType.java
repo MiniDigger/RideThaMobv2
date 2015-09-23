6168ce5d145165be08e08b5f33c4444ea0b42b22
MiniDigger <admin@minidigger.me>
2015-09-23 17:18:29 +0200

    Regen all classes to gen slime...

--- src/main/java/me/MiniDigger/RideThaMob/nms/RideAbleEntityType.java
+++ src/main/java/me/MiniDigger/RideThaMob/nms/RideAbleEntityType.java
@@ -7,13 +7,50 @@ import net.minecraft.server.v1_8_R3.EntityInsentient;
 import net.minecraft.server.v1_8_R3.EntitySkeleton;
 
 public enum RideAbleEntityType {
-	SKELETON("Skeleton", 51, EntityType.SKELETON, EntitySkeleton.class, RideAbleSkeleton.class);
+	BAT("Bat", 65, EntityType.BAT),
+	BLAZE("Blaze", 61, EntityType.BLAZE),
+	CAVESPIDER("CaveSpider", 59, EntityType.CAVE_SPIDER),
+	CHICKEN("Chicken", 93, EntityType.CHICKEN),
+	COW("Cow", 92, EntityType.COW),
+	CREEPER("Creeper", 50, EntityType.CREEPER),
+	ENDERDRAGON("EnderDragon", 63, EntityType.ENDER_DRAGON),
+	ENDERMAN("Enderman", 58, EntityType.ENDERMAN),
+	ENDERMITE("Endermite", 67, EntityType.ENDERMITE),
+	GHAST("Ghast", 56, EntityType.GHAST),
+	GIANT("Giant", 53, EntityType.GIANT),
+	GUARDIAN("Guardian", 68, EntityType.GUARDIAN),
+	HORSE("Horse", 100, EntityType.HORSE),
+	IRONGOLEM("IronGolem", 99, EntityType.IRON_GOLEM),
+	MAGMACUBE("MagmaCube", 62, EntityType.MAGMA_CUBE),
+	MUSHROOMCOW("MushroomCow", 96, EntityType.MUSHROOM_COW),
+	OCELOT("Ocelot", 98, EntityType.OCELOT),
+	PIG("Pig", 90, EntityType.PIG),
+	PIGZOMBIE("PigZombie", 57, EntityType.PIG_ZOMBIE),
+	RABBIT("Rabbit", 101, EntityType.RABBIT),
+	SHEEP("Sheep", 91, EntityType.SHEEP),
+	SILVERFISH("Silverfish", 60, EntityType.SILVERFISH),
+	SKELETON("Skeleton", 51, EntityType.SKELETON),
+	SLIME("Slime", 55, EntityType.SLIME),
+	SNOWMAN("Snowman", 97, EntityType.SNOWMAN),
+	SPIDER("Spider", 52, EntityType.SPIDER),
+	SQUID("Squid", 94, EntityType.SQUID),
+	VILLAGER("Villager", 120, EntityType.VILLAGER),
+	WITCH("Witch", 66, EntityType.WITCH),
+	WITHER("Wither", 64, EntityType.WITHER),
+	WOLF("Wolf", 95, EntityType.WOLF),
+	ZOMBIE("Zombie", 54, EntityType.ZOMBIE);
 
-	private String name;
-	private int id;
-	private EntityType entityType;
-	private Class<? extends EntityInsentient> nmsClass;
+	public static void registerEntities() {
+		for (RideAbleEntityType entity : values()) {
+			NMSUtils.registerEntity(entity.getName(), entity.getID(), entity.getNMSClass(), entity.getCustomClass());
+		}
+	}
 	private Class<? extends EntityInsentient> customClass;
+	private EntityType entityType;
+	private int id;
+	private String name;
+
+	private Class<? extends EntityInsentient> nmsClass;
 
 	private RideAbleEntityType(String name, int id, EntityType entityType, Class<? extends EntityInsentient> nmsClass,
 			Class<? extends EntityInsentient> customClass) {
@@ -24,30 +61,24 @@ public enum RideAbleEntityType {
 		this.customClass = customClass;
 	}
 
-	public String getName() {
-		return this.name;
-	}
-
-	public int getID() {
-		return this.id;
+	public Class<? extends EntityInsentient> getCustomClass() {
+		return this.customClass;
 	}
 
 	public EntityType getEntityType() {
 		return this.entityType;
 	}
 
+	public int getID() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
 	public Class<? extends EntityInsentient> getNMSClass() {
 		return this.nmsClass;
-	}
-
-	public Class<? extends EntityInsentient> getCustomClass() {
-		return this.customClass;
-	}
-
-	public static void registerEntities() {
-		for (RideAbleEntityType entity : values()) {
-			NMSUtils.registerEntity(entity.getName(), entity.getID(), entity.getNMSClass(), entity.getCustomClass());
-		}
 	}
 
 }
